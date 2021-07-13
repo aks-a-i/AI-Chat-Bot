7e4e3b940d40e1e24442b59df69e64445b627d69
aks-a-i <68985548+aks-a-i@users.noreply.github.com>
2021-07-13 09:55:16 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,31 @@
+class data():
+    account_details = [
+        {
+            "account_number" : "12345",
+            'name' : 'akshay',
+            'ifsc_code' : '0020sbin00',
+            'customer_id': '99999',
+            'address': 'ss,sss,pin-99999',
+            'balance' : '999'
+        },
+        {
+            "account_number": "123456",
+            'name': 'akshaym',
+            'ifsc_code': '0020sbin00',
+            'customer_id': '999998',
+            'address': 'sst,sss,pin-99999',
+            'balance': '9998'
+        }
+    ]
+    transaction_history = [
+        {
+            "account_number" : "12345",
+            "20/05/2020" : "1000",
+            "15/05/2020" : "2000"
+        },
+        {
+            "account_number": "123456",
+            "20/05/2020": "3000",
+            "15/05/2020": "4000"
+        }
+    ]
