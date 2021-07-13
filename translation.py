198b4ade993717a8b92100b72451afd15b4e6951
aks-a-i <68985548+aks-a-i@users.noreply.github.com>
2021-07-13 09:56:17 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,24 @@
+class helper_strings():
+
+    #greeting_list = ["hi", "hello", "how are you", "whatsup", "hey", "hii"]
+    greeting_reply = """Hello there, I am AI-CHAT-BOT
+how can i help you?
+
+1.Get your account details
+2.Get your transaction history
+3.Connect with our customer care
+"""
+    tail_end = """
+1.Get your account details
+2.Get your transaction history
+3.Connect with our customer care
+"""
+    header = """My responses are limited
+I can help you with-
+"""
+    connect_customercare = """customer care numbers
+working days only
+
+0490-9999999
+0490-8888888888
+"""
